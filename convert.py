797478c6cf4d4eb4375922b2ed492f801a586d63
tejaswini <tejaswiniathawale13@gmail.com>
2026-07-16 14:49:00 +0530

    Delete convert.py

--- convert.py
+++
@@ -1,8 +0,0 @@
-import pypandoc
-
-print("Downloading pandoc...")
-pypandoc.download_pandoc()
-
-print("Converting documentation.md to documentation.docx...")
-pypandoc.convert_file('documentation.md', 'docx', outputfile='documentation.docx')
-print("Done.")
